dd492408ab836e2a1f3babee2a03f6a9853b74b3
Francisco Javier Rubio <33929387+FJRubio67@users.noreply.github.com>
2025-07-09 11:16:28 +0100

    Update MCMC_vs_NormalApp.R

--- MCMC_vs_NormalApp.R
+++ MCMC_vs_NormalApp.R
@@ -15,7 +15,7 @@ Sigma = solve(hess)
 library(mvtnorm)
 
 set.seed(123)
-post_napp = rmvnorm(n = 1000, mean = MAP, sigma = Sigma)
+post_napp = rmvnorm(n = 10000, mean = MAP, sigma = Sigma)
 
 ##############################################################
 # Graphs for comparisons between mcmc and normal approximation
@@ -36,13 +36,13 @@ legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)
 
 # alpha
 plot(density(exp(post_napp[,3])), main = expression(alpha ~ ": MCMC vs Normal Approximation"),
-     lwd = 2, col = "blue", xlab = expression(alpha), ylab = "Density")
+     lwd = 2, col = "blue", xlab = expression(alpha), ylab = "Density", xlim = c(0,11))
 lines(density(alphapHR), lwd = 2, col = "red")
 legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)  
 
 # beta
 plot(density(exp(post_napp[,4])), main = expression(beta ~ ": MCMC vs Normal Approximation"),
-     lwd = 2, col = "blue", xlab = expression(beta), ylab = "Density")
+     lwd = 2, col = "blue", xlab = expression(beta), ylab = "Density", xlim = c(1,10))
 lines(density(betapHR), lwd = 2, col = "red")
 legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)  
 
