2a0df974e3a44b0abbdc5b553331121d4c3f1b76
Siqi-Sarah-Zheng <siqizheng017@gmail.com>
2025-07-09 10:45:57 +0100

    Update MCMC_vs_NormalApp.R

--- MCMC_vs_NormalApp.R
+++ MCMC_vs_NormalApp.R
@@ -17,6 +17,33 @@ library(mvtnorm)
 set.seed(123)
 post_napp = rmvnorm(n = 1000, mean = MAP, sigma = Sigma)
 
-# normal approximation plot for lambda
-plot(density(exp(post_napp[,1])), main = "normal approximation plot for lambda")
+##############################################################
+# Graphs for comparisons between mcmc and normal approximation
+##############################################################
+
+    
+# lambda
+plot(density(exp(post_napp[,1])), main= expression(lambda ~ ": MCMC vs Normal Approximation"),
+     lwd = 2, col = "blue", xlab=expression(lambda), ylab = "Density")
+lines(density(lambdapHR), lwd = 2, col = "red")
+legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)
+
+# kappa
+plot(density(exp(post_napp[,2])), main = expression(kappa ~ ": MCMC vs Normal Approximation"),
+     lwd = 2, col = "blue", xlab = expression(kappa), ylab = "Density")
+lines(density(kappapHR), lwd = 2, col = "red")
+legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)     
+
+# alpha
+plot(density(exp(post_napp[,3])), main = expression(alpha ~ ": MCMC vs Normal Approximation"),
+     lwd = 2, col = "blue", xlab = expression(alpha), ylab = "Density")
+lines(density(alphapHR), lwd = 2, col = "red")
+legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)  
+
+# beta
+plot(density(exp(post_napp[,4])), main = expression(beta ~ ": MCMC vs Normal Approximation"),
+     lwd = 2, col = "blue", xlab = expression(beta), ylab = "Density")
+lines(density(betapHR), lwd = 2, col = "red")
+legend("topright", c("Normal","MCMC"), col=c("blue","red"), lwd=2)  
+
 
