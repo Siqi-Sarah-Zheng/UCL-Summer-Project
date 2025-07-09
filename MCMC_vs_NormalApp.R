79634ddb704bd35f8188ced9b4f9132ba19ae532
Siqi-Sarah-Zheng <siqizheng017@gmail.com>
2025-07-09 10:44:09 +0100

    mcmc vs normal approximation

---
+++ MCMC_vs_NormalApp.R
@@ -0,0 +1,22 @@
+##############################
+# Normal Approximation
+##############################
+
+library(numDeriv)
+
+MAP = nlminb(inits, log_postHR, control = list(iter.max = 1e4))$par
+
+
+hess = hessian(func = log_postHR, x = MAP)
+
+
+Sigma = solve(hess)
+
+library(mvtnorm)
+
+set.seed(123)
+post_napp = rmvnorm(n = 1000, mean = MAP, sigma = Sigma)
+
+# normal approximation plot for lambda
+plot(density(exp(post_napp[,1])), main = "normal approximation plot for lambda")
+
