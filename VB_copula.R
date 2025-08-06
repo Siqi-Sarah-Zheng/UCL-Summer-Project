a2d1a3be95cac6c95ae4e4610600471503247a89
Siqi-Sarah-Zheng <siqizheng017@gmail.com>
2025-08-06 14:58:59 +0100

    Add files via upload

--- VB_copula.R
+++ VB_copula.R
@@ -1,4 +1,3 @@
-library(MASS)
 library(mvtnorm)
 # install.packages(c("deSolve", "survival", "devtools"))
 library(deSolve)
@@ -33,58 +32,60 @@ t_obs <- df$time[status]
 survtimes <- df$time
 
 
-# qtp3_new <- function(p, mu, par1, par2, FUN, param = "tp") {
-#   
-#   clip_prob <- function(x, eps = 1e-10) {
-#     x[x < eps]     <- eps
-#     x[x > 1 - eps] <- 1 - eps
-#     x
-#   }
-#   
-#   param <- match.arg(param, choices = c("tp", "eps", "isf"))
-#   
-#   if (param == "tp") {
-#     if (par1 > 0 && par2 > 0) {
-#       Q <- ifelse(
-#         p < par1 / (par1 + par2),
-#         mu + par1 * FUN(clip_prob(0.5 * p * (par1 + par2) / par1)),
-#         mu + par2 * FUN(clip_prob(0.5 * ((par1 + par2) * (1 + p) - 2 * par1) / par2))
-#       )
-#     } else {
-#       stop("qtp3_new: par1 and par2 must both be > 0 for 'tp'")
-#     }
-#   }
-#   
-#   if (param == "eps") {
-#     sigma <- par1
-#     gamma <- par2
-#     if (sigma > 0 && abs(gamma) < 1) {
-#       Q <- ifelse(
-#         p < 0.5 * (1 + gamma),
-#         mu + sigma * (1 + gamma) * FUN(clip_prob(p / (1 + gamma))),
-#         mu + sigma * (1 - gamma) * FUN(clip_prob((p - gamma) / (1 - gamma)))
-#       )
-#     } else {
-#       stop("qtp3_new: sigma > 0 and |gamma| < 1 are required for 'eps'")
-#     }
-#   }
-#   
-#   if (param == "isf") {
-#     sigma <- par1
-#     gamma <- par2
-#     if (sigma > 0 && gamma > 0) {
-#       Q <- ifelse(
-#         p < gamma^2 / (1 + gamma^2),
-#         mu + sigma * gamma * FUN(clip_prob(0.5 * p * (1 + gamma^2) / gamma^2)),
-#         mu + sigma       * FUN(clip_prob(0.5 * (p * (1 + gamma^2) + 1 - gamma^2))) / gamma
-#       )
-#     } else {
-#       stop("qtp3_new: sigma > 0 and gamma > 0 are required for 'isf'")
-#     }
-#   }
-#   
-#   return(Q)
-# }
+
+# I adjusted the original qtp3 function to remove the NaNs in optimisation
+qtp3_new <- function(p, mu, par1, par2, FUN, param = "tp") {
+
+  clip_prob <- function(x, eps = 1e-10) {
+    x[x < eps]     <- eps
+    x[x > 1 - eps] <- 1 - eps
+    x
+  }
+
+  param <- match.arg(param, choices = c("tp", "eps", "isf"))
+
+  if (param == "tp") {
+    if (par1 > 0 && par2 > 0) {
+      Q <- ifelse(
+        p < par1 / (par1 + par2),
+        mu + par1 * FUN(clip_prob(0.5 * p * (par1 + par2) / par1)),
+        mu + par2 * FUN(clip_prob(0.5 * ((par1 + par2) * (1 + p) - 2 * par1) / par2))
+      )
+    } else {
+      stop("qtp3_new: par1 and par2 must both be > 0 for 'tp'")
+    }
+  }
+
+  if (param == "eps") {
+    sigma <- par1
+    gamma <- par2
+    if (sigma > 0 && abs(gamma) < 1) {
+      Q <- ifelse(
+        p < 0.5 * (1 + gamma),
+        mu + sigma * (1 + gamma) * FUN(clip_prob(p / (1 + gamma))),
+        mu + sigma * (1 - gamma) * FUN(clip_prob((p - gamma) / (1 - gamma)))
+      )
+    } else {
+      stop("qtp3_new: sigma > 0 and |gamma| < 1 are required for 'eps'")
+    }
+  }
+
+  if (param == "isf") {
+    sigma <- par1
+    gamma <- par2
+    if (sigma > 0 && gamma > 0) {
+      Q <- ifelse(
+        p < gamma^2 / (1 + gamma^2),
+        mu + sigma * gamma * FUN(clip_prob(0.5 * p * (1 + gamma^2) / gamma^2)),
+        mu + sigma       * FUN(clip_prob(0.5 * (p * (1 + gamma^2) + 1 - gamma^2))) / gamma
+      )
+    } else {
+      stop("qtp3_new: sigma > 0 and gamma > 0 are required for 'isf'")
+    }
+  }
+
+  return(Q)
+}
 
 
 
@@ -116,10 +117,10 @@ mc_elbo <- function(phi, N = 10000){
   U <- pnorm(Z)
  
   eta <- cbind(
-    qtp3(U[,1], mu[1], sigma1[1], sigma2[1], FUN = qnorm),
-    qtp3(U[,2], mu[2], sigma1[2], sigma2[2], FUN = qnorm),
-    qtp3(U[,3], mu[3], sigma1[3], sigma2[3], FUN = qnorm),
-    qtp3(U[,4], mu[4], sigma1[4], sigma2[4], FUN = qnorm)
+    qtp3_new(U[,1], mu[1], sigma1[1], sigma2[1], FUN = qnorm),
+    qtp3_new(U[,2], mu[2], sigma1[2], sigma2[2], FUN = qnorm),
+    qtp3_new(U[,3], mu[3], sigma1[3], sigma2[3], FUN = qnorm),
+    qtp3_new(U[,4], mu[4], sigma1[4], sigma2[4], FUN = qnorm)
   )
   
   
@@ -194,10 +195,10 @@ Z_samples <- mvrnorm(n = 10000, mu = rep(0, 4), Sigma = R)
 U_samples <- pnorm(Z_samples)
 
 eta_samples_copula <- cbind(
-  qtp3(U_samples[,1], mu[1], sigma1[1], sigma2[1], FUN = qnorm),
-  qtp3(U_samples[,2], mu[2], sigma1[2], sigma2[2], FUN = qnorm),
-  qtp3(U_samples[,3], mu[3], sigma1[3], sigma2[3], FUN = qnorm),
-  qtp3(U_samples[,4], mu[4], sigma1[4], sigma2[4], FUN = qnorm)
+  qtp3_new(U_samples[,1], mu[1], sigma1[1], sigma2[1], FUN = qnorm),
+  qtp3_new(U_samples[,2], mu[2], sigma1[2], sigma2[2], FUN = qnorm),
+  qtp3_new(U_samples[,3], mu[3], sigma1[3], sigma2[3], FUN = qnorm),
+  qtp3_new(U_samples[,4], mu[4], sigma1[4], sigma2[4], FUN = qnorm)
 )
 
 
@@ -205,3 +206,51 @@ theta_copula_samples <- as.data.frame(exp(eta_samples_copula))
 colnames(theta_copula_samples) <- c("lambda", "kappa", "alpha", "beta")
 
 summary(theta_copula_samples)
+
+#################################################################################
+# Plots for Comparisons
+#################################################################################
+
+# Plot for lambda
+plot(density(exp(post_napp[,1])),
+     main = expression(lambda ~ ": Comparisons between Normal, MCMC, VB copula"),
+     lwd = 2, col = "blue",
+     xlab = expression(lambda), ylab = "Density")
+lines(density(lambdapHR), lwd = 2, col = "red")
+lines(density(theta_copula_samples$lambda), lwd = 2, col = "orange")
+legend("topright", c("Normal", "MCMC", "VB copula"),
+       col = c("blue", "red", "orange"), lwd = 2)
+
+# kappa
+plot(density(exp(post_napp[,2])),
+     main = expression(kappa ~ ": Comparisons between Normal, MCMC, VB copula"),
+     lwd = 2, col = "blue",
+     xlab = expression(kappa), ylab = "Density",
+     ylim = c(0, 35))
+lines(density(kappapHR), lwd = 2, col = "red")
+lines(density(theta_copula_samples$kappa), lwd = 2, col = "orange")
+legend("topright", c("Normal", "MCMC", "VB copula"),
+       col = c("blue", "red", "orange"), lwd = 2)
+
+# alpha
+plot(density(exp(post_napp[,3])),
+     main = expression(alpha ~ ": Comparisons between Normal, MCMC, VB copula"),
+     lwd = 2, col = "blue",
+     xlab = expression(alpha), ylab = "Density",
+     xlim = c(0, 11))
+lines(density(alphapHR), lwd = 2, col = "red")
+lines(density(theta_copula_samples$alpha), lwd = 2, col = "orange")
+legend("topright", c("Normal", "MCMC", "VB copula"),
+       col = c("blue", "red", "orange"), lwd = 2)
+
+# beta
+plot(density(exp(post_napp[,4])),
+     main = expression(beta ~ ": Comparisons between Normal, MCMC, VB copula"),
+     lwd = 2, col = "blue",
+     xlab = expression(beta), ylab = "Density",
+     xlim = c(1, 10))
+lines(density(betapHR), lwd = 2, col = "red")
+lines(density(theta_copula_samples$beta), lwd = 2, col = "orange")
+legend("topright", c("Normal", "MCMC", "VB copula"),
+       col = c("blue", "red", "orange"), lwd = 2)
+
